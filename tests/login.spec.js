7b6497beb2b19d528059010646d4a0f376638ce6
praveen2567 <praveenmt.kumar@gmail.com>
2025-06-05 16:17:38 +0530

    framework commit

---
+++ tests/login.spec.js
@@ -0,0 +1,61 @@
+import {test, expect} from '@playwright/test';
+
+test("Login with valid credentials", async ({page}) => {
+    // Actions
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    await page.locator("input[name='username']").fill("Admin");
+
+    await page.locator("input[name='password']").fill("admin123");
+
+    await page.locator("button[type='submit']").click();
+
+    // Assertions - verification point min 1 assertion in your test
+    await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+
+})
+
+test("Verify login with valid username and invalid password", async ({page}) => {
+    // Actions
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    await page.locator("input[name='username']").fill("Admin");
+
+    await page.locator("input[name='password']").fill("qwedfs");
+
+    await page.locator("button[type='submit']").click();
+
+    // Assertions - verification point min 1 assertion in your test
+    await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible();   
+
+})
+
+test("Verify login with invalid username and valid password", async ({page}) => {
+    // Actions
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    await page.locator("input[name='username']").fill("qwsdf");
+
+    await page.locator("input[name='password']").fill("admin123");
+
+    await page.locator("button[type='submit']").click();
+
+    // Assertions - verification point min 1 assertion in your test
+    await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible();   
+
+})
+
+test("Verify login with invalid username and invalid password", async ({page}) => {
+    // Actions
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    await page.locator("input[name='username']").fill("qwsdf");
+
+    await page.locator("input[name='password']").fill("pshdf");
+
+    await page.locator("button[type='submit']").click();
+
+    // Assertions - verification point min 1 assertion in your test
+    await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible();   
+
+})
